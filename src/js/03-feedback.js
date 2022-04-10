c8b97f257e2c3a37c77031652892dfe125dd3497
Vlad Pomynchuk <pomynchuk123@gmail.com>
2022-04-10 17:06:37 +0300

    add task - 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+
+const {
+  elements: { email, message },
+} = formRef;
+
+const onFormChange = e => {
+  const savedData = {};
+
+  savedData.email = email.value;
+  savedData.message = message.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(savedData));
+};
+
+if (localStorage.getItem('feedback-form-state')) {
+  const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  email.value = savedData.email;
+  message.value = savedData.message;
+}
+
+const onSubmitForm = e => {
+  e.preventDefault();
+
+  console.log({
+    email: email.value,
+    message: message.value,
+  });
+
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+};
+
+formRef.addEventListener('input', throttle(onFormChange, 500));
+formRef.addEventListener('submit', onSubmitForm);
