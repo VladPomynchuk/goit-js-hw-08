efd671e2dfc03c6a988797005cb675c2e9b95a8a
Vlad Pomynchuk <pomynchuk123@gmail.com>
2022-04-10 15:59:07 +0300

    add task - 1, 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const galleryEl = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>`,
+  )
+  .join('');
+
+galleryRef.insertAdjacentHTML('afterbegin', galleryEl);
+
+// const galleryLinkRefs = document.querySelectorAll('.gallery__item');
+// galleryLinkRefs.forEach(galleryLinkRef => {
+//   const onGalleryLink = e => {
+//     e.preventDefault();
+
+//     console.log('click');
+//   };
+//   galleryLinkRef.addEventListener('click', onGalleryLink);
+// });
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const onTimeSave = e => {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      localStorage.setItem('videoplayer-current-time', seconds);
+    })
+    .catch(function (error) {
+      console.log('error');
+    });
+};
+
+player.on('timeupdate', throttle(onTimeSave, 1000));
+
+let CurrentTime = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(CurrentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
